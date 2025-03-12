d6604633125f0b7fd518a7a5bcd03626f6689222
rudresh.sigmato <rudresh.sigmato@gmail.com>
2025-03-12 13:41:55 +0530

    productship component chnages

--- components/products/ProductShips.tsx
+++ components/products/ProductShips.tsx
@@ -7,19 +7,17 @@ interface ProductShipsImageProps {
   height?: number;
   className?: string;
 }
-
 const features = [
   { icon: '/images/la_award.svg', title: 'FREE DELIVERY', description: 'Lorem ipsum dolor sit amet, consectetur adipi elit.' },
   { icon: '/images/la_award.svg', title: '100% SECURE PAYMENT', description: 'Lorem ipsum dolor sit amet, consectetur adipi elit.' },
   { icon: '/images/la_award.svg', title: 'DAILY OFFERS', description: 'Lorem ipsum dolor sit amet, consectetur adipi elit.' },
   { icon: '/images/la_award.svg', title: 'QUALITY GUARANTEE', description: 'Lorem ipsum dolor sit amet, consectetur adipi elit.' },
 ];
-
 const ProductShips: React.FC<ProductShipsImageProps> = ({
 
 }) => {
   return (
-    <div className="bg-pink-50 xl:h-[317px] py-14 md:py-0">
+    <div className="bg-pink-50 xl:h-[317px] py-14 md:py-0 ">
       <div className="w-full h-full flex items-center justify-center">
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 text-center mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
           {features.map((feature, index) => (
