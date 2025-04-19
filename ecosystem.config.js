ddaa84ddbbfa6f3074470e230539feccd40d39d2
SigmatoDev <nuthan.raghunath@sigmato.com>
2025-04-19 22:52:10 +0530

    addedm

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,6 +7,7 @@ module.exports = {
         env: {
           NODE_ENV: "production",
           NEXTAUTH_URL: "https://staging.kelayaa.com",
+          NEXTAUTH_SECRET: "MIwt4h2XCQoRZ/VyVeO97nBnpRJJvnGzb3C01OMtZHA=",  // put your actual secret here
           NEXTAUTH_TRUST_HOST: "true",
         },
       },
