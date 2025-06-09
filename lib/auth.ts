a5220efbd8d1d4368a73c6f101b3a69f2f3dd825
Dinesh Dipti Ranjan Bhukta <bhuktadinesh19@gmail.com>
2025-06-09 11:59:13 +0530

    `Refactor auth configuration and add cookie domain settings`

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,19 +1,19 @@
-import bcrypt from 'bcryptjs';
-import NextAuth from 'next-auth';
-import CredentialsProvider from 'next-auth/providers/credentials';
+import bcrypt from "bcryptjs";
+import NextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
 
-import dbConnect from './dbConnect';
-import UserModel from './models/UserModel';
+import dbConnect from "./dbConnect";
+import UserModel from "./models/UserModel";
 
 export const config = {
   providers: [
     CredentialsProvider({
       credentials: {
         email: {
-          type: 'email',
+          type: "email",
         },
         password: {
-          type: 'password',
+          type: "password",
         },
       },
       async authorize(credentials) {
@@ -25,7 +25,7 @@ export const config = {
         if (user) {
           const isMatch = await bcrypt.compare(
             credentials.password as string,
-            user.password,
+            user.password
           );
           if (isMatch) {
             return user;
@@ -35,11 +35,24 @@ export const config = {
       },
     }),
   ],
+  // 👇 Add this block for cookie domain setting
+  cookies: {
+    sessionToken: {
+      name: `next-auth.session-token`,
+      options: {
+        domain: `.kelayaa.com`, // ✅ This ensures cookies work across subdomains
+        path: "/",
+        httpOnly: true,
+        sameSite: "lax",
+        secure: true, // ✅ Required for HTTPS (staging & prod)
+      },
+    },
+  },
   // custom pages for sign in and register
   pages: {
-    signIn: '/signin',
-    newUser: '/register',
-    error: '/error',
+    signIn: "/signin",
+    newUser: "/register",
+    error: "/error",
   },
   callbacks: {
     async jwt({ user, trigger, session, token }: any) {
@@ -49,15 +62,15 @@ export const config = {
           email: user.email,
           name: user.name,
           isAdmin: user.isAdmin,
-          mobileNumber:user.mobileNumber
+          mobileNumber: user.mobileNumber,
         };
       }
-      if (trigger === 'update' && session) {
+      if (trigger === "update" && session) {
         token.user = {
           ...token.user,
           email: session.user.email,
           name: session.user.name,
-          mobileNumber:session.user.mobileNumber
+          mobileNumber: session.user.mobileNumber,
         };
       }
       return token;
