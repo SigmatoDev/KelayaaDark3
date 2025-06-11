181e6538cc9c7c4b4712b2227485efb8a925a42b
Dinesh Dipti Ranjan Bhukta <bhuktadinesh19@gmail.com>
2025-06-11 12:10:57 +0530

    `Update StatusPage component layout with minor spacing adjustments`

--- app/status/[id]/page.tsx
+++ app/status/[id]/page.tsx
@@ -250,7 +250,7 @@ const StatusPage = () => {
         hideProgressBar={false}
       />
       <div className="flex items-center justify-center h-screen">
-        <div className="bg-white p-8 rounded-lg shadow-lg w-full max-w-md">
+        <div className="bg-white p-8 my-3 sm:my-6 rounded-lg shadow-lg w-full max-w-md">
           {loading ? (
             <div className="flex justify-center items-center space-x-2">
               <div className="w-8 h-8 border-4 border-t-4 border-gray-300 border-t-transparent rounded-full animate-spin"></div>
